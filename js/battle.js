4a92efb68825334841fba350a78ebdc478c8522d
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-28 14:35:12 -0700

    Refactored Scoreboard & created staging page

--- js/battle.js
+++ js/battle.js
@@ -3,27 +3,29 @@ if (!window.jQuery) {
   alert('Sorry! Your browser is super outdated, and might have viruses. Please upgrade to Firefox, Chrome, or a newer version of IE - immediately.');
 }
 
-jQuery.template = (function ($) {
+;(function ($) {
     var templates = {};
 
-    return function (target, values) {
+    function jQueryDotTemplate (target, values) {
         if (!templates[target]) {
             templates[target] = $(target).html();
         }
 
-        var html = templates[target];
-        for (var i in values) {
-            var pattern = new RegExp('\\$' + i, 'g');
-            html = html.replace(pattern, values[i]);
-        }
+        var $html = $('<div>' + templates[target] + '</div>');
 
-        var $html = $(html);
         for (var i in values) {
+            $html.find('.attribute-' + i).attr(i, values[i]);
+            $html.find('.class-' + i).addClass(values[i]);
+            $html.find('.href-' + i).attr('href', values[i]);
+            $html.find('.src-' + i).attr('src', values[i]);
+            $html.find('.target-' + i).attr('target', values[i]);
             $html.find('.text-' + i).text(values[i]);
         }
 
-        return $html;
+        return $html.children();
     }
+
+    $.template = jQueryDotTemplate;
 })(jQuery);
 
 var photoCloud = {
@@ -470,75 +472,6 @@ var photoCloud = {
     window.org = 'fp';
   }
 
-  // Political Scoreboard
-  $.getJSON('https://spreadsheets.google.com/feeds/list/1-hBOL7oNJXWvUdhK0veiybSXaYFUZu1aNUuRyNeaUmg/default/public/values?alt=json', function(response) {
-    var $isotope = $('.isotope');
-
-    // Parse & sort by weight
-    var players = [];
-    for (var i in response.feed.entry) {
-      var player = response.feed.entry[i];
-
-      player = {
-        name: player.gsx$name.$t,
-        organization: player.gsx$organization.$t,
-        image: player.gsx$imagepleasedontedit.$t,
-        weight: player.gsx$weight.$t,
-        team: player.gsx$team.$t,
-        size: player.gsx$size.$t
-      };
-
-      if (player.team) {
-        players.push(player);
-      }
-    }
-
-    players = players.sort(function(a, b) {
-      return b.weight - a.weight;
-    });
-
-    // Create elements
-    var $els = $('<div>');
-    for (var i in players) {
-      if (i > 50) {
-        break;
-      }
-
-      var player = players[i],
-          $el = $.template('#player', player);
-
-      $el.data('meta', player);
-
-      $el.appendTo($els);
-    }
-    $els.appendTo($isotope);
-
-    // Sort based on teams.
-    regenerateWeights(players);
-
-    // Initialize isotope.
-    $isotope.isotope({
-      getSortData: {
-        weight: function (el) {
-          var meta = $(el).data('meta');
-          return -meta.weightGenerated || -meta.weight;
-        }
-      },
-      itemSelector: '.politician',
-      masonry: {
-        columnWidth : 150,
-        isFitWidth  : true
-      },
-      sortBy: 'weight'
-    });
-
-    // Resort based on teams, every resize.
-    $(window).on('resize', function onResize() {
-      regenerateWeights(players);
-      $isotope.isotope('updateSortData').isotope();
-    });
-  });
-
   // Support IE9+
   function getInternetExplorerVersion()
   // Returns the version of Internet Explorer or a -1
@@ -563,149 +496,6 @@ var photoCloud = {
       $('body').addClass('oldie');
       $('input, textarea').placeholder();
     }
-  }
-
-
-
-  // Political Scoreboard logic
-  function regenerateWeights(players) {
-    var across   = Math.floor($('#political').width() / 150),
-        eligible = Math.ceil(across / 3);
-
-    // We can't sort with less than 3 columns.
-    if (across < 3) {
-      return _.each(players, function (player) {
-        player.weightGenerated = null;
-      });
-    }
-
-    // Create a map, for hit detection.
-    var map = [];
-    _.times(across, function() {
-      map.push([]);
-    });
-
-    var position  = { x: 0, y: 0 },
-        remaining = players.length,
-        weight    = 10000;
-
-    // Add flag to each player.
-    _.each(players, function (player) {
-      player.positioned = false;
-    });
-
-
-    // Place each player.
-    while (remaining > 0) {
-      var availability = getSpatialAvailability(position, map);
-      if (!availability) {
-        position = movePosition(position, map);
-        continue;
-      }
-
-      var player,
-          query = {
-            positioned: false
-          };
-
-      if (availability === 'small') {
-        query.size = 'small';
-      }
-
-      if (position.x <= eligible - 1) {
-        query.team = 'team-cable';
-      } else if (position.x >= across - eligible) {
-        query.team = 'team-internet';
-      } else {
-        query.team = 'undecided';
-      }
-
-      player = _.findWhere(players, query);
-
-      if (!player && query.team === 'undecided') {
-        if ((position.x + 1) / across > .5) {
-          query.team = 'team-internet';
-        } else {
-          query.team = 'team-cable';
-        }
-      }
-
-      player = _.findWhere(players, query);
-
-      if (!player) {
-        delete query.team;
-        player = _.findWhere(players, query);
-      }
-
-      player.weightGenerated = weight--;
-      player.positioned = true;
-
-      map[position.x][position.y] = true;
-      if (player.size === 'large') {
-        map[position.x + 1][position.y] = true;
-        map[position.x][position.y + 1] = true;
-        map[position.x + 1][position.y + 1] = true;
-      }
-
-      // printMap(position, map);
-
-      position = movePosition(position, map);
-
-      remaining--;
-    }
-  }
-
-  function printMap (position, map) {
-    var width = map.length;
-
-    console.log('');
-
-    var msg;
-    for (var y = 0, yMax = map[0].length; y < yMax; y++) {
-      msg = y + ': ';
-      for (var x = 0, xMax = map.length; x < xMax; x++) {
-        var value = map[x][y];
-
-        var character;
-        if (x === position.x && y === position.y) {
-          character = '* ';
-        } else if (value === undefined) {
-          character = '- ';
-        } else if (value === true) {
-          character = 'x ';
-        }
-
-        msg += character;
-      }
-      console.log(msg + '\n');
-    }
-  }
-
-  function movePosition (position, map) {
-    position.x++;
-
-    if (position.x === map.length) {
-      position.x = 0;
-      position.y++;
-    }
-
-    return position;
-  }
-
-  function getSpatialAvailability (position, map) {
-    if (map[position.x][position.y]) {
-      return false;
-    }
-
-    if (
-      !map[position.x][position.y + 1] &&
-      map[position.x + 1] &&
-      !map[position.x + 1][position.y] &&
-      !map[position.x + 1][position.y + 1]
-    ) {
-      return 'large';
-    }
-    return 'small';
   }
 
 })(jQuery);
---
+++ js/scoreboard.js
@@ -0,0 +1,212 @@
+jQuery(function($) {
+    // Political Scoreboard
+    var $isotope = $('.isotope');
+    var spreadsheetKey = $isotope.data('spreadsheet-key');
+    var spreadsheetUrl = 'https://spreadsheets.google.com/feeds/list/' + spreadsheetKey + '/default/public/values?alt=json';
+
+    $.getJSON(spreadsheetUrl, function(response) {
+        // Parse & sort by weight
+        var players = [];
+        for (var i in response.feed.entry) {
+            var player = response.feed.entry[i];
+
+            player = {
+                name: player.gsx$name.$t,
+                organization: player.gsx$organization.$t,
+                image: '/images/scoreboard/' + player.gsx$imagepleasedontedit.$t,
+                weight: player.gsx$weight.$t,
+                team: player.gsx$team.$t,
+                size: player.gsx$size.$t
+            };
+
+            if (player.team) {
+                players.push(player);
+            }
+        }
+
+        players = players.sort(function(a, b) {
+            return b.weight - a.weight;
+        });
+
+        // Create elements
+        var $els = $('<div>');
+        for (var i in players) {
+            var player = players[i],
+                $el = $.template('#player', player);
+
+            $el.data('meta', player);
+
+            $el.appendTo($els);
+        }
+        $els.appendTo($isotope);
+
+        // Sort based on teams.
+        regenerateWeights(players);
+
+        // Initialize isotope.
+        $isotope.isotope({
+            getSortData: {
+                weight: function(el) {
+                    var meta = $(el).data('meta');
+                    return -meta.weightGenerated || -meta.weight;
+                }
+            },
+            itemSelector: '.politician',
+            masonry: {
+                columnWidth: 150,
+                isFitWidth: true
+            },
+            sortBy: 'weight'
+        });
+
+        // Resort based on teams, every resize.
+        $(window).on('resize', function onResize() {
+            regenerateWeights(players);
+            $isotope.isotope('updateSortData').isotope();
+        });
+    });
+
+    // Political Scoreboard logic
+    function regenerateWeights(players) {
+        var across = Math.floor($('#political').width() / 150),
+            eligible = Math.ceil(across / 3);
+
+        // We can't sort with less than 3 columns.
+        if (across < 3) {
+            return _.each(players, function(player) {
+                player.weightGenerated = null;
+            });
+        }
+
+        // Create a map, for hit detection.
+        var map = [];
+        _.times(across, function() {
+            map.push([]);
+        });
+
+        var position = {
+                x: 0,
+                y: 0
+            },
+            remaining = players.length,
+            weight = 10000;
+
+        // Add flag to each player.
+        _.each(players, function(player) {
+            player.positioned = false;
+        });
+
+
+        // Place each player.
+        while (remaining > 0) {
+            var availability = getSpatialAvailability(position, map);
+            if (!availability) {
+                position = movePosition(position, map);
+                continue;
+            }
+
+            var player,
+                query = {
+                    positioned: false
+                };
+
+            if (availability === 'small') {
+                query.size = 'small';
+            }
+
+            if (position.x <= eligible - 1) {
+                query.team = 'team-cable';
+            } else if (position.x >= across - eligible) {
+                query.team = 'team-internet';
+            } else {
+                query.team = 'undecided';
+            }
+
+            player = _.findWhere(players, query);
+
+            if (!player && query.team === 'undecided') {
+                if ((position.x + 1) / across > .5) {
+                    query.team = 'team-internet';
+                } else {
+                    query.team = 'team-cable';
+                }
+            }
+
+            player = _.findWhere(players, query);
+
+            if (!player) {
+                delete query.team;
+                player = _.findWhere(players, query);
+            }
+
+            player.weightGenerated = weight--;
+            player.positioned = true;
+
+            map[position.x][position.y] = true;
+            if (player.size === 'large') {
+                map[position.x + 1][position.y] = true;
+                map[position.x][position.y + 1] = true;
+                map[position.x + 1][position.y + 1] = true;
+            }
+
+            // printMap(position, map);
+
+            position = movePosition(position, map);
+
+            remaining--;
+        }
+    }
+
+    function printMap(position, map) {
+        var width = map.length;
+
+        console.log('');
+
+        var msg;
+        for (var y = 0, yMax = map[0].length; y < yMax; y++) {
+            msg = y + ': ';
+            for (var x = 0, xMax = map.length; x < xMax; x++) {
+                var value = map[x][y];
+
+                var character;
+                if (x === position.x && y === position.y) {
+                    character = '* ';
+                } else if (value === undefined) {
+                    character = '- ';
+                } else if (value === true) {
+                    character = 'x ';
+                }
+
+                msg += character;
+            }
+            console.log(msg + '\n');
+        }
+    }
+
+    function movePosition(position, map) {
+        position.x++;
+
+        if (position.x === map.length) {
+            position.x = 0;
+            position.y++;
+        }
+
+        return position;
+    }
+
+    function getSpatialAvailability(position, map) {
+        if (map[position.x][position.y]) {
+            return false;
+        }
+
+        if (!map[position.x][position.y + 1] &&
+            map[position.x + 1] &&
+            !map[position.x + 1][position.y] &&
+            !map[position.x + 1][position.y + 1]
+        ) {
+            return 'large';
+        }
+
+        return 'small';
+    }
+});
