9079491a40eb8d723355f40535cf395e7e8c0ce3
Chris Antaki <ChrisAntaki@gmail.com>
2014-09-10 15:40:48 -0700

    Count: Chilled out animation, commented out infographic link

--- js/count.js
+++ js/count.js
@@ -3,7 +3,7 @@ jQuery(function($) {
         var baseline = 4400000;
         var calls = 80603;
         var comments = 451387;
-        var numAnim = new countUp("animated-count", baseline, baseline + calls + comments, 0, 3.3);
+        var numAnim = new countUp("animated-count", baseline, baseline + calls + comments, 0, 5.87);
         numAnim.start();
     }, 2500);
 });
