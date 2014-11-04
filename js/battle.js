44518a0a793ac984a0c4795636beb5e986a547ca
Jeff L <jeff@rubbingalcoholic.com>
2014-11-03 16:23:04 -0800

    vigil info

--- js/battle.js
+++ js/battle.js
@@ -342,6 +342,37 @@ var loaded_wall = false;
     }
   });
 
+  $('#subscribe').click(function(e) {
+      e.preventDefault();
+
+      $.ajax('https://action.fightforthefuture.org/api/movements/1/salsa', {
+          type: 'POST',
+          data: {
+              'csrfmiddlewaretoken': 'Hs3unIicI0ztSKnejzAn8CmPS4YnBCpW',
+              'member[language]': 'en',
+              'salsa': true,
+              'language_id': 'en',
+              'member[movement_id]': 1,
+              'member[user_agent]': '',
+              'member[ip_address]': '',
+              'guard': '',
+              'action': 1,
+              'hp_enabled': true,
+              'hp_disabled': false,
+              'member[first_name]': '',
+              'member[email]': $('#email').val(),
+              'member[street_address]': '',
+              'member[postcode]': '',
+              'tag': 'nocompromiseprotest'
+          },
+          complete: function(xhr, status) {
+              $('.signup').css('visibility', 'hidden');
+              $('.signup-thanks').show();
+          }
+      });
+
+  });
+
   if ($('a.break').length) {
     $('a.break').click(function(e) {
       e.preventDefault();
