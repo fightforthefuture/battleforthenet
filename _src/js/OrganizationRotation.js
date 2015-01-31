dbd1510846f4e4f88e38bbed4963577d9a1af6b9
Chris Antaki <ChrisAntaki@gmail.com>
2015-01-30 18:21:42 -0800

    Navigation: Added donation link, and renamed Scoreboard to Your Congressperson

--- _src/js/OrganizationRotation.js
+++ _src/js/OrganizationRotation.js
@@ -4,30 +4,42 @@ function OrganizationRotation() {
 
 OrganizationRotation.prototype.addEventListeners = function() {
     var loc = window.location.href;
-    random_org = null;
+    var randomOrg = null;
+    var chosenOrg = null;
     if (loc.indexOf('org=') == -1) {
         var coin_toss = Math.random();
         if (coin_toss < .20) {
-            random_org = 'fp';
+            randomOrg = 'fp';
         } else if (coin_toss < .60) {
-            random_org = 'dp';
+            randomOrg = 'dp';
         } else {
-            random_org = 'fftf';
+            randomOrg = 'fftf';
         }
     }
 
-    if (loc.indexOf('org=fp') != -1 || random_org == 'fp') {
+    if (loc.indexOf('org=fp') != -1 || randomOrg == 'fp') {
         document.getElementById('org').value = 'fp';
         document.getElementById('randomize_disclosure').style.display = 'none';
         document.getElementById('fp_disclosure').style.display = 'block';
-    } else if (loc.indexOf('org=dp') != -1 || random_org == 'dp') {
+
+        chosenOrg = 'fp';
+    } else if (loc.indexOf('org=dp') != -1 || randomOrg == 'dp') {
         document.getElementById('org').value = 'dp';
         document.getElementById('randomize_disclosure').style.display = 'none';
         document.getElementById('dp_disclosure').style.display = 'block';
-    } else if (loc.indexOf('org=fftf') != -1 || random_org == 'fftf') {
+
+        chosenOrg = 'dp';
+    } else if (loc.indexOf('org=fftf') != -1 || randomOrg == 'fftf') {
         document.getElementById('org').value = 'fftf';
         document.getElementById('randomize_disclosure').style.display = 'none';
         document.getElementById('fftf_disclosure').style.display = 'block';
+
+        chosenOrg = 'fftf';
+    }
+
+    var donationLinks = document.querySelectorAll('header a.donate');
+    for (var i = 0; i < donationLinks.length; i++) {
+        donationLinks[i].href = donationLinks[i].getAttribute('href-' + chosenOrg);
     }
 };
 
