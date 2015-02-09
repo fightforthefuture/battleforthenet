1f5baed6fb202cd1ea4c65ae8db33d4aab681801
Chris Antaki <ChrisAntaki@gmail.com>
2015-02-09 13:18:19 -0800

    Sped up load time by simplifying form

--- js/battle.js
+++ js/battle.js
@@ -72,26 +72,8 @@ var TeamInternetSection = require('./TeamInternetSection');
     // Let's selectively bust browser caches
     var buster = '?buster=' + Date.now();
 
-    var ajaxQueue = new Queue({
-        callback: function() {
-            var pleaseWaitNode = document.querySelector('#battle .please-wait');
-            pleaseWaitNode.parentNode.removeChild(pleaseWaitNode);
-
-            new PetitionForm({
-                allPoliticians: global.ajaxResponses.politicians,
-                formTemplate: global.ajaxResponses.formTemplate,
-                geography: global.ajaxResponses.geography,
-                target: '#battle .form-wrapper'
-            });
-
-            // Rotate organizations
-            new OrganizationRotation();
-
-            // Add more sections
-            setTimeout(loadMoreSections, 400);
-        },
-        remaining: 3
-    });
+    // allPoliticians: global.ajaxResponses.politicians,
+    // geography: global.ajaxResponses.geography,
 
     var LiveURLs = {
         geography: 'https://fftf-geocoder.herokuapp.com',
@@ -109,44 +91,21 @@ var TeamInternetSection = require('./TeamInternetSection');
     // }
 
     new AJAX({
-        url: URLs.geography,
-        success: function(e) {
-            var json = JSON.parse(e.target.responseText);
-            global.ajaxResponses.geography = json;
-            ajaxQueue.tick();
-        }
-    });
-
-    new AJAX({
-        url: URLs.politiciansOnGoogle,
-        success: function(e) {
-            try {
-                var json = JSON.parse(e.target.responseText);
-                global.ajaxResponses.politicians = json.feed.entry;
-                ajaxQueue.tick();
-            } catch (e) {
-                grabPoliticiansFromBackup();
-            }
-        },
-        error: grabPoliticiansFromBackup
-    });
-
-    function grabPoliticiansFromBackup() {
-        new AJAX({
-            url: URLs.politicians,
-            success: function(e) {
-                var json = JSON.parse(e.target.responseText);
-                global.ajaxResponses.politicians = json.feed.entry;
-                ajaxQueue.tick();
-            }
-        });
-    }
-
-    new AJAX({
         url: 'templates/PetitionForm.html' + buster,
         success: function(e) {
-            global.ajaxResponses.formTemplate = e.target.responseText;
-            ajaxQueue.tick();
+            var pleaseWaitNode = document.querySelector('#battle .please-wait');
+            pleaseWaitNode.parentNode.removeChild(pleaseWaitNode);
+
+            new PetitionForm({
+                formTemplate: e.target.responseText,
+                target: '#battle .form-wrapper'
+            });
+
+            // Rotate organizations
+            new OrganizationRotation();
+
+            // Add more sections
+            setTimeout(loadMoreSections, 400);
         }
     });
 
@@ -816,88 +775,19 @@ var Template = require('./Template');
 
 function PetitionForm(params) {
     // Params
-    this.allPoliticians = params.allPoliticians;
     this.formTemplate = params.formTemplate;
-    this.geography = params.geography;
     this.target = params.target;
 
     this.DOMNode = document.querySelector(this.target);
 
-    this.selectPoliticians();
     this.render();
     this.addEventListeners();
 }
 
-PetitionForm.prototype.selectPoliticians = function() {
-    this.politicians = [];
-
-    if (
-        this.geography.country.iso_code === 'US' &&
-        this.geography.subdivisions &&
-        this.geography.subdivisions[0] &&
-        this.geography.subdivisions[0].names &&
-        this.geography.subdivisions[0].names.en
-    ) {
-        var stateName = this.geography.subdivisions[0].names.en;
-        this.politicians = this.allPoliticians.filter(function(politician) {
-            return (
-                (politician.gsx$state.$t === stateName)
-                &&
-                (politician.gsx$organization.$t === 'Senate')
-            );
-        });
-    }
-
-    if (this.politicians.length === 0) {
-        var teamCable = this.allPoliticians.filter(function(politician) {
-            return (
-                (politician.gsx$team.$t === 'team-cable')
-            )
-        });
-
-        this.politicians = [];
-        this.politicians[0] = teamCable[Math.floor(Math.random() * teamCable.length) - 1];
-        while (!this.politicians[1] || this.politicians[0] === this.politicians[1]) {
-            this.politicians[1] = teamCable[Math.floor(Math.random() * teamCable.length) - 1];
-        }
-    }
-};
-
 PetitionForm.prototype.render = function() {
-    this.DOMNode.innerHTML = Template(this.formTemplate, {
-        politicians: this.politicians.map(function(politician) {
-            var team = politician.gsx$team.$t;
-
-            var stance = 'undecided';
-            if (team === 'team-cable') {
-                stance = 'anti internet';
-            } else if (team === 'team-internet') {
-                stance = 'pro internet';
-            }
-
-            var url = 'http://';
-            if (politician.gsx$subdomain.$t) {
-                url += politician.gsx$subdomain.$t;
-            } else {
-                url += politician.gsx$first.$t + politician.gsx$name.$t;
-            }
-            if (politician.gsx$team.$t.trim() === 'team-internet') {
-                url += '.savesthe.net';
-            } else {
-                url += '.breaksthe.net';
-            }
-            url = url.toLowerCase();
-
-            return {
-                image: 'images/scoreboard/' + politician.gsx$imagepleasedontedit.$t,
-                name: politician.gsx$name.$t,
-                url: url,
-                stance: stance,
-                team: team
-            };
-        })
-    });
+    this.DOMNode.innerHTML = Template(this.formTemplate, {});
     this.DOMNode.className = this.DOMNode.className.replace(/loading/, ' ');
+
 };
 
 PetitionForm.prototype.validateEmail = function(email) {
@@ -970,12 +860,12 @@ PetitionForm.prototype.addEventListeners = function() {
             success: function(e) {}
         });
         if (ga) ga('send', 'event', 'form', 'submit', 'email');
-       
+
         petitionFormNode.style.display = 'none';
         politiciansNode.style.display = 'none';
         phoneCallFormNode.style.display = 'block';
         disclaimerNode.style.display = 'none';
-      
+
 
     }, false);
 
@@ -993,7 +883,7 @@ PetitionForm.prototype.addEventListeners = function() {
         }
 
         var url =
-            'https://call-congress.fightforthefuture.org/create?' + 
+            'https://call-congress.fightforthefuture.org/create?' +
             'campaignId=' + campaignId + '&' +
             'userPhone=' + phoneNumber + '&' +
             'zipcode=' + postalCode;
