ffa3145feb7ffe8b01a9d2677609c11fdba257b9
holmesworcester <hwilson@gmail.com>
2017-06-19 18:15:01 -0400

    More content changes

--- ts/petition-copy.tsx
+++ ts/petition-copy.tsx
@@ -16,11 +16,11 @@ export class PetitionCopy extends React.Component<Props, State> {
 		return (
 			<div className="petition-copy">
 				<p>
-					Comcast &amp; Verizon want the power to control what we see &amp; do online.
-					The FCC will let them, unless we stop it.
+					Comcast &amp; Verizon want to end net neutrality so they can control what we see &amp; do online.
+					In 82 days, the FCC will let them, unless we stop it.
 					This is a battle for the Internet's future.
 					{" "}
-					<em>Before you do anything else, send a letter to the FCC now!</em>
+					<em>Before you do anything else, send a letter to the FCC &amp; Congress now!</em>
 				</p>
 			</div>
 		);
