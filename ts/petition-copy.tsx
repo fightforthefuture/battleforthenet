7c78d8177e81aeba0b2ce5b5a2c368d77f01d64f
holmesworcester <hwilson@gmail.com>
2017-08-11 16:31:37 -0400

    Updated frontpage text

--- ts/petition-copy.tsx
+++ ts/petition-copy.tsx
@@ -23,15 +23,14 @@ export class PetitionCopy extends React.Component<Props, State> {
 				<p>
 					{ etsy
 						? `FCC Chairman Pai wants to repeal existing net neutrality rules that allow Etsy sellers to turn their creative passion into a business. Without these protections, Etsy sellers will be forced to choose between paying for priority access or losing sales in the internet slow lane.`
-						: <span>Comcast, Verizon and AT&T want to end net neutrality so they can charge extra fees & control what we see & do online.
-							On <a href="/july12/">July 12</a>, we organized over <a href="https://www.fightforthefuture.org/news/2017-07-13-update-historic-day-of-action-for-net-neutrality/">2,000,000 comments</a> to stop them.
-						  This is a battle for the Internet's future.</span>
+						: <span>Comcast, Verizon and AT&T want to end net neutrality so they can control what we see & do online.
+							First, they want to gut FCC rules. Then, they plan to pass bad legislation that allows extra fees, throttling & censorship.</span>
 					}
 					{" "}
 					<em>
 						{ etsy
 				  		? "Send a message to the FCC and Congress urging them to protect net neutrality and microbusinesses. "
-							: "If you haven't yet, send a letter to the FCC & Congress now!"
+							: "Time is running out to stop them. Write Congress & the FCC now!"
 						}
 					</em>
 				</p>
