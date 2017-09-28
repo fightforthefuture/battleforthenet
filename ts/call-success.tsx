1648a43f1f44ee2f17e81a60863925fe5c078f3d
shu zOMG chen <shu.chen@freelancedreams.com>
2017-09-28 14:09:29 -0700

    Update call tool success modal copy

--- ts/call-success.tsx
+++ ts/call-success.tsx
@@ -12,20 +12,21 @@ interface Props {
 interface State {
 }
 
+const content_msg = `
+I support Title II net neutrality rules and I urge you to oppose the FCC's
+plan to repeal them. Specifically, I'd like you to oppose Ajit Pai's
+confirmation as FCC Chair, and contact the FCC to demand that they abandon
+their current plan.
+`;
+
 export class CallSuccess extends React.Component<Props, State> {
 	render() {
 		return (
 			<div>
 				<h2>Calling you now!</h2>
-				<h4>
-					Press * after each call to start the next one.
-					Hang up whenever you like, but
-					<em>more calls = more impact.</em>
-				</h4>
 
-				<p><em>You can introduce yourself and say,</em></p>
-				<p>I’m concerned about net neutrality. Millions of people spoke out for the strong net neutrality rules we currently have because they protect us from ISPs like Comcast and Verizon that want to block, throttle, and charge us extra fees to access information equally online.</p>
-				<p>I support Title II net neutrality rules and I urge you to oppose the FCC’s plan to repeal them.</p>
+				<p><em>Introduce yourself and say,</em></p>
+				<p>{ content_msg }</p>
 				{ this.props.swap ? "" : <AfterActionFooter org={this.props.org} zip={this.props.zip} /> }
 			</div>
 		);
