272b46002cb2d4de5253d987385725c9408fb547
holmesworcester <hwilson@gmail.com>
2017-10-12 13:28:42 -0400

    text tweaks to simplify this view
    @mikemorris just wanted to call your attention to these changes. The
    change to the way the privacy policy displays is the most important, I
    think.

--- ts/templates.tsx
+++ ts/templates.tsx
@@ -13,11 +13,11 @@ export function CallActionFormTemplate(props:CallActionFormProps, state:CallActi
 				<h3>This may be your last chance to stop ISPs from #$%@'ing with your Internet.</h3>
 				<p>
 					FCC Chair Ajit Pai is about to break America's net neutrality
-					rules—meaning ISPs like Comcast & Verizon will be free to block apps,
+					rules—meaning companies like Comcast & Verizon will be able to block apps,
 					slow websites, and charge fees to control what you see & do online.
 					{" "}
-					<em>This is utter garbage, but only Congressional Republicans can stop
-					him, and we may have only 13 days to do it. Can you call Congress
+					<em>This is utter garbage, but only Congress can stop
+					him now, and we may have only 13 days. Can you call Congress
 					right now?</em>
 				</p>
 				<div>
@@ -27,9 +27,7 @@ export function CallActionFormTemplate(props:CallActionFormProps, state:CallActi
 					<button className="btn">Call Congress</button>
 				</div>
 				<p>
-					(We’ll connect you and provide a suggested script of what to say)
-				</p>
-				<p className="disclaimer">{ ctx.campaign.disclaimer }{" "}<a href="/privacy" target="_blank">Privacy Policy</a></p>
+					(We’ll connect you and provide a suggested script of what to say. { ctx.campaign.disclaimer }{" "}<a href="/privacy" target="_blank">Privacy Policy</a>)</p>
 			</form>
 		</div>
 	);
