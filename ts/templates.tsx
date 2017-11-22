992b4a29c9d4e25e32e9ece1ff6e3694f374cf3a
shu zOMG chen <shu.chen@freelancedreams.com>
2017-11-22 11:29:51 -0800

    Add petition back to flow, refactor template back out

--- ts/templates.tsx
+++ ts/templates.tsx
@@ -2,11 +2,70 @@ import * as React from 'react';
 import * as _ from 'lodash';
 
 import {CallActionFormProps, CallActionFormState, CallActionFormContext} from './call-action-form';
+import {PetitionFormProps, PetitionFormState, PetitionFormContext} from './petition-form';
 import {LeaderboardProps, LeaderboardState, LeaderboardContext, LeaderboardReferrer} from './leaderboard';
 import {CallSuccessProps} from './call-success';
+import {Disclaimer} from './disclaimer';
 import {AfterActionFooter} from './after-action-footer';
 import {classes} from './utils';
 
+
+export function PetitionFormTemplate(props:PetitionFormProps, state:PetitionFormState, ctx:PetitionFormContext) {
+	return (
+		<div>
+			<form className="bftn-form petition-form" onSubmit={ctx.onSubmit}>
+				<img className="arrow" src="/images/red-arrow.png" />
+				<div className="form-wrapper">
+					<div>
+						<input name="input_name" placeholder="Name" autoComplete="name" required value={state.input_name} onChange={ctx.handleInputChange} />
+					</div>
+					<div>
+						<input name="input_email" placeholder="E-mail" required value={state.input_email} onChange={ctx.handleInputChange} />
+					</div>
+					<div className="address-line">
+						<input name="input_address" className="address" placeholder="Address" required value={state.input_address} onChange={ctx.handleInputChange} />
+						{" "}
+						<input name="input_zip" className="zip" placeholder="Zip" required value={state.input_zip} onChange={ctx.handleInputChange} />
+					</div>
+					{props.swap ? "" : <div><input name="input_phone" className="phone" placeholder="Phone # (to volunteer)" value={state.input_phone} onChange={ctx.handleInputChange} /> </div> }
+					<div className="letter">
+						<textarea ref={ctx.setTextarea} name="input_comment" required value={state.input_comment} onChange={ctx.handleInputChange} onFocus={ctx.onTextareaFocus} ></textarea>
+						<button onClick={ctx.onResetClick} className="reset">Clear and write your own</button>
+					</div>
+					{ props.etsy
+						? <div className="etsy-shop-link"><input type="text" placeholder="Etsy Shop Link" name="input_etsy_shop" value={state.input_etsy_shop} onChange={ctx.handleInputChange} /></div>
+						: ""
+					}
+					<button className="btn">Send Letter</button>
+				</div>
+				{ state.error ? (
+						<div className="form-error">
+							<span className="oi" data-glyph="warning" title="previous" aria-hidden="true"></span>
+							{" "}{state.error}
+						</div>
+					) : null }
+				{ props.etsy ? <span className="opt-in"><input type="checkbox" name="input_opt_in" checked={state.input_opt_in} onChange={ctx.handleInputChange} /> </span> : "" }
+				<Disclaimer org={props.org} optIn={props.etsy} swap={props.swap}/>
+			</form>
+			<div className="petition-copy">
+				<p>
+					{ props.etsy
+						? `FCC Chairman Pai wants to repeal existing net neutrality rules that allow Etsy sellers to turn their creative passion into a business. Without these protections, Etsy sellers will be forced to choose between paying for priority access or losing sales in the internet slow lane.`
+						: <span>Comcast, Verizon and AT&T want to end net neutrality so they can control what we see & do online.
+							First, they want to gut FCC rules. Then, they plan to pass bad legislation that allows extra fees, throttling & censorship.</span>
+					}
+					{" "}
+					<em>
+						{ props.etsy
+							? "Send a message to the FCC and Congress urging them to protect net neutrality and microbusinesses. "
+							: "Time is running out to stop them. Write Congress & the FCC now!"
+						}
+					</em>
+				</p>
+			</div>
+		</div>
+	);
+}
 
 export function CallActionFormTemplate(props:CallActionFormProps, state:CallActionFormState, ctx:CallActionFormContext) {
 	return (
