1f1b33fe6a6a2d9a3ffd80f040c94a502e75d8a8
Ken Mickles <ken@37i.net>
2017-12-05 13:05:49 -0500

    More break the internet tweaks
    * Add FB + Twitter sharing text
    * Make it clear that phone is optional
    * Add disclosure text
    * Add post-save share modal
    * Update subheadline text
    * Add volunteer checkbox
    * Update campaign ID

--- breaktheinternet/js/app.js
+++ breaktheinternet/js/app.js
@@ -1,4 +1,30 @@
 $(function(){
+  var showModal = function() {
+    $('.modal-wrapper').fadeIn();
+    $('body').addClass('modal-open');
+  };
+
+  var hideModal = function() {
+    $('.modal-wrapper').fadeOut();
+    $('body').removeClass('modal-open');
+  };
+
+  // toggle large audience checkbox
+  $('input[name=volunteer').click(function(e){
+    var isChecked =  $(this).is(':checked');
+    var $div = $('.action-comment');
+
+    if (isChecked) {
+      $div.show(100, function(){
+        $div.find('input').focus();
+      })
+    }
+    else {
+      $div.slideUp(100);
+    }
+  });
+
+  // submit form
   $('form').submit(function(e){
     e.preventDefault();
     
@@ -10,19 +36,24 @@ $(function(){
 
     var $button = $form.find('button')
     var buttonText = $button.text()
-    $button.text('Loading...');
+    $button.text('Saving...');
     
     $.post($form.attr('action'), params, function(response) {
       if (response.data) {
-        $form.slideUp('fast', function(){
-          $('.form-success').show()
-        });
+        showModal();
       }
       else {
-        $form.find('input').removeAttr('disabled');
         $('.form-error').show();
-        $button.text(buttonText);
       }
+
+      $form.find('input').removeAttr('disabled');
+      $button.text(buttonText);
     });
+  });
+
+  // close modal
+  $('.modal .close').click(function(e) {
+    e.preventDefault();
+    hideModal();
   });
 });
