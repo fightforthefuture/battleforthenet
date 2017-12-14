86cf55794834b8cc442766b9bc93e330c787afc3
shu zOMG chen <shu.chen@freelancedreams.com>
2017-12-14 11:18:18 -0800

    Use static politician data

--- ts/political-scoreboard.tsx
+++ ts/political-scoreboard.tsx
@@ -165,7 +165,8 @@ export function getGeocode(): Promise<string> {
 export function getPoliticians(): Promise<PoliticiansSet> {
 	return ajaxPromise({
 		// url: "https://spreadsheets.google.com/feeds/list/1n6ZuVMbfBdu3MvYutScnnD8k8B2IzqX9woBY-2PLlIM/default/public/values?alt=json",
-		url: "https://cache.battleforthenet.com/v2/politicians.json",
+		// url: "https://cache.battleforthenet.com/v2/politicians.json",
+		url: "/js/politicians.json",
 		method: "get",
 		json: true
 	}).then(function(response: Response) {
