d2f986d29263b1aa48f5b88316908a073d837843
Ken Mickles <ken@37i.net>
2018-01-31 13:39:38 -0500

    First pass at business outreach scoreboard

---
+++ public/business/js/app.js
@@ -0,0 +1,180 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var STATES = [
+    "Alaska",
+    "Alabama",
+    "Arkansas",
+    "Arizona",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "District of Columbia",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Iowa",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Massachusetts",
+    "Maryland",
+    "Maine",
+    "Michigan",
+    "Minnesota",
+    "Missouri",
+    "Mississippi",
+    "Montana",
+    "North Carolina",
+    "North Dakota",
+    "Nebraska",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "Nevada",
+    "New York",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Virginia",
+    "Vermont",
+    "Washington",
+    "Wisconsin",
+    "West Virginia",
+    "Wyoming"
+  ];
+
+  var formatNumber = function(x) {
+    return x ? x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '';
+  };
+
+  var pluralize = function(number, singular, plural=null){
+    if (parseFloat(number) === 1.0) {
+      return number + ' ' + singular;
+    }
+    else {
+      if (!plural) {
+        plural = singular + 's';
+      }
+
+      return formatNumber(number) + ' ' + plural;
+    }
+  };
+
+  var shuffle = function(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  Vue.filter('formatNumber', formatNumber);
+  Vue.filter('pluralize', pluralize);
+
+  var app = new Vue({
+    el: '#app',
+
+    data: function() {
+      return {
+        states: STATES,
+        selectedState: null,
+        isLoaded: false,
+        businesses: [],
+        tweetCount: 0,
+        modalVisible: false
+      }
+    },
+
+    watch: {
+      tweetCount: function(val) {
+        if (val === 4) {
+          this.showModal()
+        }
+      }
+    },
+
+    computed: {
+      localBusinesses: function() {
+        var self = this;
+        return this.businesses.filter(function(b){
+          return b.state === self.selectedState;
+        });
+      },
+    },
+
+    created: function() {
+      this.geocodeSelectedState();
+      this.fetchBusinesses();
+    },
+
+    methods: {
+      geocodeSelectedState: function() {
+        var self = this;
+        this.$http.get('https://fftf-geocoder.herokuapp.com').then(function(response){
+          if (response.ok) {
+            var geo = response.body;
+
+            if (
+              geo.country.iso_code === 'US' &&
+              geo.subdivisions &&
+              geo.subdivisions[0] &&
+              geo.subdivisions[0].names &&
+              geo.subdivisions[0].names.en
+            ) {
+              self.selectedState = geo.subdivisions[0].names.en;
+            }
+          }
+        });
+      },
+
+      fetchBusinesses: function() {
+        var self = this;
+        this.$http.get('https://data.battleforthenet.com/businesses.json').then(function(response){
+          if (response.ok) {
+            self.businesses = response.body;
+            shuffle(self.businesses);
+            self.isLoaded = true;
+          }
+        });
+      },
+
+      showModal: function() {
+        this.modalVisible = true;
+        document.querySelector('body').classList.add('modal-open');
+      },
+
+      hideModal: function() {
+        this.modalVisible = false;
+        document.querySelector('body').classList.remove('modal-open');
+      }
+    }
+  });
+
+  Vue.component('business-card', {
+    template: '#business-card-template',
+    props: [ 'business' ],
+    methods: {
+      tweetURL: function(business) {
+        var tweetText = "@" + business.twitter + " i am tweeting at you!"
+        return 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText);
+      },
+
+      openTweetURL: function(business) {
+        this.$parent.tweetCount += 1
+        var url = this.tweetURL(business);
+        window.open(url, '_blank');
+      }
+    }
+  });
+});
