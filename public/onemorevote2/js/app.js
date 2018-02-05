3c9732bbcbe4c6e7a4ea44be0584163f6bf58a6d
Ken Mickles <ken@37i.net>
2018-02-05 14:20:18 -0500

    Add remaining /onemorevote/ content to new design

--- public/onemorevote2/js/app.js
+++ public/onemorevote2/js/app.js
@@ -9,7 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         zipCode: null,
         phone: null,
         isLoading: false,
-        formMessage: null
+        formMessage: null,
+        modalVisible: false
       }
     },
 
@@ -40,8 +41,8 @@ document.addEventListener("DOMContentLoaded", function() {
             this.name = null;
             this.email = null;
             this.zipCode = null;
-            self.formMessage = "Thanks! We'll be in touch :)";
-            // self.showModal();
+            self.formMessage = null;
+            self.showModal();
           }
           else {
             self.formMessage = "That didn't work for some reason :(";
@@ -52,6 +53,54 @@ document.addEventListener("DOMContentLoaded", function() {
           self.formMessage = "That didn't work for some reason :(";
         })
       },
+
+      showModal: function() {
+        this.modalVisible = true;
+        document.querySelector('body').classList.add('modal-open');
+      },
+
+      hideModal: function() {
+        this.modalVisible = false;
+        document.querySelector('body').classList.remove('modal-open');
+      },
+
+      getMetaContent: function(name) {
+        var el = document.querySelector('meta[name="' + name + '"]') || document.querySelector('meta[property="' + name + '"]');
+        
+        if (el) {
+          return el.getAttribute('content');
+        }
+
+        return null;
+      },
+
+      openPopup: function(url, title='popup', w=600, h=500) {
+        // Fixes dual-screen position
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+          newWindow.focus();
+        }
+      },
+
+      shareOnFacebook: function() {
+        var url = this.getMetaContent('og:url');
+        this.openPopup('https://www.facebook.com/sharer.php?u=' + encodeURIComponent(url), 'facebook');
+      },
+
+      shareOnTwitter: function() {
+        var tweetText = this.getMetaContent('twitter:description') + ' ' + this.getMetaContent('twitter:url');
+        this.openPopup('https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText), 'twitter');
+      }
     }
   })
 });
