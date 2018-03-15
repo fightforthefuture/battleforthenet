ac3cbff39c713b4a1837df9a80e13e5913a5a2a1
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-03-15 13:12:10 -0700

    Updated body copy

--- src/ts/templates.tsx
+++ src/ts/templates.tsx
@@ -62,8 +62,7 @@ export function CallActionFormTemplate(props:CallActionFormProps, state:CallActi
 					<h3>We're running out of time to save net neutrality.</h3>
 				}
 				<p>
-				The FCC voted to kill net neutrality and let ISPs like Comcast ruin the web with throttling, censorship, and new fees. Congress has 60 legislative days to overrule them and save the Internet using the Congressional Review Act (CRA), but we still need #OneMoreVote to win in the Senate. Can you write Congress now?
-					{" "}
+				The FCC voted to kill net neutrality and let ISPs like Comcast ruin the web with throttling, censorship, and new fees. Congress has 60 legislative days to overrule them and save the Internet using the Congressional Review Act (CRA), but we still need #OneMoreVote to win in the Senate. <em>Can you write Congress now?<em>
 				</p>
 				<div className="flex">
 					<input className={classes(state.error && "error")} name="input_phone" type="tel" placeholder="Enter your phone #" value={state.input_phone} onChange={ctx.onChange} />
