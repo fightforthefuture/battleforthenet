a696a51e0477a3d9aa737c1a4141c7ffae6b0af7
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-03-15 13:15:24 -0700

    Added back line 66

--- src/ts/templates.tsx
+++ src/ts/templates.tsx
@@ -63,6 +63,7 @@ export function CallActionFormTemplate(props:CallActionFormProps, state:CallActi
 				}
 				<p>
 				The FCC voted to kill net neutrality and let ISPs like Comcast ruin the web with throttling, censorship, and new fees. Congress has 60 legislative days to overrule them and save the Internet using the Congressional Review Act (CRA), but we still need #OneMoreVote to win in the Senate. <em>Can you write Congress now?<em>
+					{" "}
 				</p>
 				<div className="flex">
 					<input className={classes(state.error && "error")} name="input_phone" type="tel" placeholder="Enter your phone #" value={state.input_phone} onChange={ctx.onChange} />
