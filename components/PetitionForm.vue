f7b1d46b4a571d338cc8df535811e91c747c51d4
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-05-08 12:16:41 -0400

    Updated copy

--- components/PetitionForm.vue
+++ components/PetitionForm.vue
@@ -161,13 +161,13 @@ export default {
       address: null,
       zipCode: null,
       phone: null,
-      comments: `The FCC vote to destroy the Net Neutrality protections on Dec. 14 cannot stand.
+      comments: `Last December's FCC vote to destroy the Net Neutrality protections cannot stand.
 
 I’m calling on you to work with your colleagues to use the Congressional Review Act to pass a "resolution of disapproval" reversing the FCC's vote.
 
-The FCC's Dec. 14 decision willfully ignored the outcry from tens of millions of people, and it abdicated the FCC's responsibility to protect the internet from ISP blocking and discrimination. The FCC has injured our economy and free speech in just one action, all without so much as a single public hearing.
+The FCC's December decision willfully ignored the outcry of tens of millions of people, and it abdicated the FCC's responsibility to protect the internet from ISP blocking and discrimination. The FCC has injured our economy and free speech in just one action, all without so much as a single public hearing.
 
-We need members of Congress to stand up for the open internet and for the digital civil rights of their constituents now. Please co-sponsor, sign the discharge petition, and vote for the CRA to pass a Resolution of Disapproval overturning the FCC's December 14 "Restoring Internet Freedom" vote.
+We need members of Congress to stand up for the open internet and for the digital rights of their constituents now. Please co-sponsor, sign the discharge petition for, and vote for the CRA Resolution of Disapproval that would overturn the FCC's December "Restoring Internet Freedom" vote.
 
 Thank you.`
     }
