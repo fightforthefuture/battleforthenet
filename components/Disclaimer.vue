f44b5094ba3cbb17e0256a48e49d0b7efc74d6ce
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-05-08 12:20:38 -0400

    Added longer disclaimer for DP

--- components/Disclaimer.vue
+++ components/Disclaimer.vue
@@ -1,7 +1,7 @@
 <template>
   <p class="disclaimer">
     <small v-if="org == 'dp'">
-      <a target="_blank" href="https://www.demandprogress.org">Demand Progress</a> will contact you about important net neutrality updates.
+      <a target="_blank" href="https://www.demandprogress.org">Demand Progress</a> will email you updates, and you can unsubscribe at any time. If you enter your number (it's optional) we will follow up by SMS. Message &amp; data rates apply. You can always text STOP to stop receiving messages.
       <a target="_blank" href="/privacy">Privacy Policy</a>
     </small>
     <small v-else-if="org == 'fp'">
