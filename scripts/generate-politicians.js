2c153879ba34ecd33de8be0ea8b9fac7965bc8a6
Ken Mickles <ken@37i.net>
2018-05-09 01:47:12 -0400

    Prevent McCain from being re-added to scoreboard

--- scripts/generate-politicians.js
+++ scripts/generate-politicians.js
@@ -4,7 +4,8 @@ const path = require('path')
 
 async function main() {
   const { data } = await axios.get('https://data.battleforthenet.com/politicians.json')
-  fs.writeFileSync(path.resolve(__dirname, '../assets/data/politicians.json'), JSON.stringify(data, null, 2))
+  const politicians = data.filter(p => p.biocode !== 'M000303')
+  fs.writeFileSync(path.resolve(__dirname, '../assets/data/politicians.json'), JSON.stringify(politicians, null, 2))
 }
 
 main().catch(console.error)
