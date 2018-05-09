c8777e5305bb05724447307a45c623e52d9ccd7f
Ken Mickles <ken@37i.net>
2018-05-09 01:03:18 -0400

    Update sentry.io config

--- plugins/raven.js
+++ plugins/raven.js
@@ -3,7 +3,11 @@ import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
 import settings from '~/config'
 
-Raven
-  .config(settings.sentryDSN)
-  .addPlugin(RavenVue, Vue)
-  .install()
+export default ({ app }) => {
+  if (process.env.NODE_ENV !== 'production') return
+
+  Raven
+    .config(settings.sentryDSN)
+    .addPlugin(RavenVue, Vue)
+    .install()
+}
