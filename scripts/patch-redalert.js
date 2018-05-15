b7e453cb5004788489448e8d902bcf4ce2d286c4
Ken Mickles <ken@37i.net>
2018-05-14 21:57:43 -0400

    Fix typo

--- scripts/patch-redalert.js
+++ scripts/patch-redalert.js
@@ -4,4 +4,4 @@ const path = require('path')
 const filePath = path.resolve(__dirname, '../dist/redalert/index.html')
 const html = fs.readFileSync(filePath, 'utf8')
 
-fs.writeFileSync(filePath, html.replace(/<head>/, "<head><script>if(document.referrer.match(/pornhub\.com/)location.href='/'</script>"))
+fs.writeFileSync(filePath, html.replace(/<head>/, "<head><script>if(document.referrer.match(/pornhub\.com/))location.href='/'</script>"))
