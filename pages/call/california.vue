41efb100619edc1c5c69f14e72ded60033fdc64a
Ken Mickles <ken@37i.net>
2018-06-11 15:25:22 -0400

    Add some home page content to the California call page

--- pages/call/california.vue
+++ pages/call/california.vue
@@ -1,8 +1,8 @@
 <style lang="scss" scoped>
 .page-header {
-  @include big-screen {
-    min-height: calc(100vh - #{$footer-height});
-  }
+  // @include big-screen {
+  //   min-height: calc(100vh - #{$footer-height});
+  // }
 
   h1 {
     margin-bottom: 2rem;
@@ -20,21 +20,50 @@
 </style>
 
 <template>
-  <section class="page-header">
-    <div class="container">
-      <h1>{{ $lt('title') }}</h1>
-      <div v-html="$lt('body_html')" class="intro"></div>
-      <call-form page="california"></call-form>
-    </div>
-  </section>
+  <div>
+    <section class="page-header">
+      <div class="container">
+        <h1>{{ $lt('title') }}</h1>
+        <div v-html="$lt('body_html')" class="intro"></div>
+        <call-form page="california"></call-form>
+      </div>
+    </section>
+    <section id="net-neutrality">
+      <div class="container">
+        <h2 class="section-title">{{ $t('pages.index.sections.net-neutrality.title') }}</h2>
+        <div v-html="$t('pages.index.sections.net-neutrality.body_html')"></div>
+        <video-roll></video-roll>
+      </div>
+    </section>
+    <section id="team-internet">
+      <div class="container">
+        <h2 class="section-title">{{ $t('pages.index.sections.team-internet.title') }}</h2>
+        <div v-html="$t('pages.index.sections.team-internet.body_html')"></div>
+        <team-internet></team-internet>
+      </div>
+    </section>
+    <section id="team-cable">
+      <div class="container">
+        <h2 class="section-title">{{ $t('pages.index.sections.team-cable.title') }}</h2>
+        <div v-html="$t('pages.index.sections.team-cable.body_html')"></div>
+        <team-cable></team-cable>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 import CallForm from '~/components/CallForm'
+import VideoRoll from '~/components/VideoRoll'
+import TeamInternet from '~/components/TeamInternet'
+import TeamCable from '~/components/TeamCable'
 
 export default {
   components: {
-    CallForm
+    CallForm,
+    VideoRoll,
+    TeamInternet,
+    TeamCable
   },
 
   methods: {
