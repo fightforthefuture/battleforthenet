577e4c6e0624077ab53536cd37e7df26bebcbc9c
Ken Mickles <ken@37i.net>
2018-06-13 11:03:51 -0400

    Update TeamCable logos

--- components/TeamCable.vue
+++ components/TeamCable.vue
@@ -1,8 +1,46 @@
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: row wrap;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    width: 50%;
+    padding: 1.5rem 2rem;
+
+    &.verizon {
+      padding-top: 0.5rem;
+    }
+
+    img {
+      object-fit: contain;
+    }
+  }
+
+  @include big-screen {
+    margin: 0 -8rem;
+
+    li {
+      width: 25%;
+      padding: 2rem;
+
+      &.verizon {
+        padding-top: 1.1rem;
+      }
+    }
+  }
+}
+</style>
+
 <template>
-  <ul class="logo-cloud">
-    <li><img src="~/assets/images/problem-comcast.png" alt="Comcast"></li>
-    <li><img src="~/assets/images/problem-verizon.png" alt="Verizon"></li>
-    <li><img src="~/assets/images/problem-att.png" alt="ATT"></li>
-    <li><img src="~/assets/images/problem-spectrum.png" alt="Spectrum"></li>
+  <ul>
+    <li><img src="~/assets/images/att-logo.svg" alt="ATT"></li>
+    <li><img src="~/assets/images/comcast-logo.svg" alt="Comcast"></li>
+    <li class="verizon"><img src="~/assets/images/verizon-logo.svg" alt="Verizon"></li>
+    <li><img src="~/assets/images/spectrum-logo.svg" alt="Spectrum"></li>
   </ul>
 </template>
