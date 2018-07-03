1d43c1a434fe0a52b3d583799acdc5f8fc32d9cc
Ken Mickles <ken@37i.net>
2018-07-03 14:49:18 -0400

    Add business fields to PetitionForm

---
+++ components/FancyToggle.vue
@@ -0,0 +1,73 @@
+<style lang="scss">
+.fancy-toggle {
+  color: #6f6393;
+  background-color: #302a44;
+  border-radius: $border-radius;
+  padding: .3rem;
+  display: flex;
+  align-items: center;
+  width: 10rem;
+  height: 4rem;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 1.3rem;
+  position: relative;
+
+  @include mobile {
+    width: 150px;
+  }
+
+  .bg {
+    position: absolute;
+    top: 0.3rem;
+    bottom: 0.3rem;
+    right: 0.3rem;
+    width: 48%;
+    background-color: #493f62;
+    z-index: 1;
+    border-radius: $border-radius * 1.2;
+    transition: background-color .2s, transform .1s;
+  }
+
+  &.yes .bg {
+    transform: translate3d(-95%, 0, 0);
+    background-color: #18beb4;
+  }
+
+  span {
+    display: inline-block;
+    flex: 1;
+    transition: all .2s;
+    z-index: 2;
+
+    &:hover {
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+
+  &.yes span.yes,
+  &.no span.no {
+    color: #fff;
+  }
+}
+</style>
+
+<template>
+  <div class="fancy-toggle" :class="on ? 'yes' : 'no'">
+    <div class="bg"></div>
+    <span class="yes" @click="$emit('change', true)">Yes</span>
+    <span class="no" @click="$emit('change', false)">No</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    on: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
--- components/PetitionForm.vue
+++ components/PetitionForm.vue
@@ -63,9 +63,22 @@
     }
   }
 
+  .biz-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.8rem;
+
+    label {
+      font-weight: bold;
+      text-align: left;
+      padding: 0 .5rem;
+    }
+  }
+
   .btn-large {
     font-size: 2.6rem;
-    margin: 0;
+    margin: 1.2rem 0 0 0;
     letter-spacing: 0.3rem;
   }
 
@@ -97,6 +110,14 @@
           <textarea v-model="comments" ref="comments"></textarea>
           <a href="#" class="clear btn" @click.prevent="clearComments()">{{ $lt('clear_comments') }}</a>
         </div>
+        <div class="biz-row">
+          <label>{{ $lt('business_owner_label') }}</label>
+          <fancy-toggle :on="isBusinessOwner" @change="isBusinessOwner = !isBusinessOwner"></fancy-toggle>
+        </div>
+        <div v-if="isBusinessOwner" class="flex-row">
+          <input v-model.trim="companyName" type="text" :placeholder="$lt('company_name_placeholder')">
+          <input v-model.trim="companyURL" type="text" :placeholder="$lt('company_url_placeholder')">
+        </div>
         <button class="btn btn-block btn-large btn-cta" :disabled="isSending">
           <span v-if="isSending">{{ $lt('button_loading') }}</span>
           <span v-else>{{ $lt('button_cta') }}</span>
@@ -120,13 +141,19 @@
 import { mapState } from 'vuex'
 import { pingCounter, sendToMothership, startTextFlow } from '~/assets/js/helpers'
 import CallForm from '~/components/CallForm'
+import FancyToggle from '~/components/FancyToggle'
+import axios from 'axios'
 
 // battle-for-the-net-action-4
 const petitionId = '25488448-4124-4359-8873-d1ef731ea5f4'
 
+// BFNN petition
+const bizPetitionId = '94ac8142-99b8-411f-83af-133d02649af1'
+
 export default {
   components: {
-    CallForm
+    CallForm,
+    FancyToggle
   },
 
   props: {
@@ -145,7 +172,10 @@ export default {
       modalVisible: false,
       name: null,
       email: null,
-      comments: this.$lt('default_letter')
+      comments: this.$lt('default_letter'),
+      isBusinessOwner: false,
+      companyName: null,
+      companyURL: null
     }
   },
 
@@ -239,12 +269,35 @@ export default {
           this.startTextFlow()
         }
 
+        if (this.companyName && this.companyURL) {
+          this.signBusinessPetition()
+        }
+
         pingCounter('email')
       }
       catch (err) {
         self.isSending = false
         self.errorMessage = "That didn't work for some reason :("
       }
+    },
+
+    async signBusinessPetition() {
+      return axios.post(
+        `https://fv8xpw9hri.execute-api.us-east-1.amazonaws.com/v1/petitions/${bizPetitionId}/signatures`,
+        {
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          zip_code: this.zipCode,
+          comments: this.comments,
+          custom: {
+            company: this.companyName,
+            company_url: this.companyURL,
+          },
+          tags: ['bfnn', 'net-neutrality'],
+          source: this.$route.query.source
+        }
+      )
     },
 
     resetForm() {
@@ -255,6 +308,9 @@ export default {
       this.zipCode = null
       this.phone = null
       this.comments = this.$lt('default_letter')
+      this.isBusinessOwner = false
+      this.companyName = null
+      this.companyURL = null
     },
 
     startTextFlow() {
