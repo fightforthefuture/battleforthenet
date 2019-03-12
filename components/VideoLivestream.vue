cfe9c706c6a5c74cb1b0b7b2b00bc50ad1d451f4
shoshi <shoshiroberts@gmail.com>
2019-03-12 11:01:20 -0400

    Add livestream

---
+++ components/VideoLivestream.vue
@@ -0,0 +1,21 @@
+<style lang="scss" scoped>
+.video-wrapper {
+  margin-top: 2rem;
+}
+.video-wrapper iframe {
+  min-height: 360px;
+  width: 100%;
+
+  @include mobile {
+    min-height: 230px;
+  }
+}
+</style>
+
+<template>
+  <div class="video-wrapper">
+    <iframe src="https://player.vimeo.com/video/323205772" frameborder="0"
+            webkitallowfullscreen mozallowfullscreen allowfullscreen>
+    </iframe>
+  </div>
+</template>
